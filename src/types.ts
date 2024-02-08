d1b43ea2544b88ee72dd9d0e6a8c045940e54032
pietrobs <pietroschiavinato.dev@gmail.com>
2024-02-08 16:46:35 -0300

    files

---
+++ src/types.ts
@@ -0,0 +1,85 @@
+/**
+ * @pattern "^[\\w#-]+$""
+ * @minLength 1
+ * @examples this-is-an-energy-farm-id
+ */
+type CommonId = number;
+
+/**
+ * @minimum 1
+ * @maximun 31
+ * @examples 5
+ */
+type MonthDay = number;
+
+/**
+ * @minimum 1
+ * @examples 5
+ */
+type PositiveNumber = number;
+
+type ValueWithUnit<T,U> = {
+    value: T;
+    unit: U;
+}
+
+type kWp = ValueWithUnit<PositiveNumber, "kWp">
+type kW = ValueWithUnit<PositiveNumber, "kW">
+
+type GenerationCapacity = {
+    total:  kWp
+    sold: kWp
+    remaining: kWp
+    legal: kW
+    maxLegal?: kW
+    remainingLegal?: kW
+}
+
+/**
+ * @minItems 12
+ * @maxItems 12
+ */
+type ArrayWith12Positions<T> = [T]
+
+type EnergyFarmModel = {
+    id?: CommonId;
+    discoBillReadingDay: MonthDay;
+    consortiumId: CommonId;
+    discoPassword: string
+    discoUsername: string
+    fullName: string
+    discoConsumerUnitId: CommonId
+    type: 'solar' | 'hydro' | 'biogas'
+    capacity: GenerationCapacity
+    openingDate: Date
+    numberOfPanels: PositiveNumber
+    monthlyGenerationForecast: {
+        unit: "kWh",
+        value: ArrayWith12Positions<PositiveNumber>
+    }
+}
+
+export type CreateEnergyFarmRequest = {
+    body: EnergyFarmModel & {
+        id: CommonId;
+    }
+}
+
+
+// ************************************************************
+
+/**
+ *  @asType integer 
+ */
+type integer = number;
+
+export type Person = {
+    planet: "earth";
+    name: string;
+    age: integer;
+    address: string;
+}
+
+
+
+
